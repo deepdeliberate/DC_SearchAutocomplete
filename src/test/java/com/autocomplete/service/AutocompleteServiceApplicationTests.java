e50268574bacbe2d2a7c5ae50303094dbf747d4d
Naman Deep <namandeep444@gmail.com>
2026-01-07 10:59:33 +0530

    Initial Spring Boot Setup for autocomplete Service

---
+++ src/test/java/com/autocomplete/service/AutocompleteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocomplete.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompleteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
